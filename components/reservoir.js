9b88599ce4532a493177286fc2271b1a70724274
Holland Pleskac <hollandpleskac@gmail.com>
2021-11-19 11:02:33 -0800

    ui improvements on the reservoir representation

--- components/reservoir.js
+++ components/reservoir.js
@@ -1,3 +1,4 @@
+// add borders (improve ui)
 import React from 'react'
 
 const Reservoir = () => {
@@ -6,9 +7,8 @@ const Reservoir = () => {
       <p className='mb-1' >Oroville Lake</p>
       <div className='relative' >
 
-        <div className='bg-gray-200' style={{ height: 100, width: 80 }} >
-        </div>
-        <div className='absolute z-10' style={{ top: 20 }} >
+        <GrayBackground />
+        <div className='absolute z-10' style={{ top: 20, left: -5 }} >
           <RedLine />
         </div>
         <div className='absolute z-10 bottom-0' >
@@ -20,10 +20,20 @@ const Reservoir = () => {
   )
 }
 
+const GrayBackground = (params) => {
+  return (
+    <div className='bg-gray-200 border-2 border-gray-400' style={{ height: 100, width: 80 }} >
+      <p className='text-sm absolute' style={{ top: -10, left: -40 }} >4823</p>
+      <p className='text-sm absolute' style={{ bottom: -10, left: -15 }} >0</p>
+    </div>
+  )
+}
+
+
 const RedLine = () => {
   return (
     <div className='flex items-center' >
-      <div className='bg-red-400' style={{ width: 80, height: 2, marginRight: 10 }} ></div>
+      <div className='bg-red-400' style={{ width: 90, height: 2, marginRight: 5 }} ></div>
       <p className='text-sm' >4125</p>
     </div>
   )
@@ -31,9 +41,9 @@ const RedLine = () => {
 
 const BlueBar = () => {
   return (
-    <div className='relative bg-green-400' >
+    <div className='relative' >
       <p className='text-sm absolute' style={{ marginLeft: 80, top: -10, left: 10 }} >3200</p>
-      <div className='bg-blue-400' style={{ width: 80, height: 40 }} >
+      <div className='bg-blue-400' style={{ width: 70, height: 40, marginLeft: 5 }} >
       </div>
     </div>
   )

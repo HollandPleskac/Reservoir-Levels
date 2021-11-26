d89b967ee9dd6284b6a8faf62df98750d97f8a59
Holland Pleskac <hollandpleskac@gmail.com>
2021-11-26 08:45:31 -0800

    reading data from the smart contract

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
+import ContractContext from '../context/contractContext'
+import { ethers } from 'ethers'
 import Reservoir from '../components/Reservoir'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import SideBar from '../components/SideBar'
 import AlertsSignup from '../components/AlertsSignup'
 import ProgressBar from '../components/ProgressBar'
+
+const decimals = ethers.BigNumber.from(10).pow(18)
 
 const HomePage = () => {
   return (
@@ -28,12 +32,7 @@ const MainContent = () => {
       <div className='flex flex-col flex-grow' >
         <ProgressBar />
         <div className='flex-grow flex flex-col justify-center' >
-          <div className='flex justify-center mb-4' >
-            <div className='mr-48' >
-              <Reservoir maxHeight={100} historicalHeight={75} currentHeight={50} name='Oroville Lake' />
-            </div>
-            <Reservoir maxHeight={100} historicalHeight={80} currentHeight={50} name='Trinity Lake' />
-          </div>
+          <ReservoirsContainer/>
           <NextPayout />
         </div>
       </div>
@@ -41,6 +40,37 @@ const MainContent = () => {
     </div >
   )
 }
+
+const ReservoirsContainer = () => {
+  const contractCtx = useContext(ContractContext)
+  const [orovilleHeight, setOrovilleHeight] = useState()
+  const [trinityHeight, setTrinityHeight] = useState()
+
+  useEffect(() => {
+    if (contractCtx.contractAddress && contractCtx.Contract) {
+      const fetchHeights = async () => {
+        const orovilleHt = await contractCtx.getOrovilleLakeHeight()
+        const trinityHt = await contractCtx.getTrinityLakeHeight()
+        console.log('oroville Height', orovilleHt.div(decimals).toNumber())
+        console.log('trinity Height', trinityHt.div(decimals).toNumber())
+        setOrovilleHeight(orovilleHt.div(decimals).toNumber())
+        setTrinityHeight(trinityHt.div(decimals).toNumber())
+      }
+
+      fetchHeights()
+    }
+  }, [contractCtx])
+
+  return (
+    <div className='flex justify-center mb-4' >
+      <div className='mr-48' >
+        <Reservoir maxHeight={100} historicalHeight={75} currentHeight={orovilleHeight} name='Oroville Lake' />
+      </div>
+      <Reservoir maxHeight={100} historicalHeight={80} currentHeight={trinityHeight} name='Trinity Lake' />
+    </div>
+  )
+}
+
 
 const NextPayout = () => {
   return (
